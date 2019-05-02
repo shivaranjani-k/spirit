d37c8c881662df34d580f6d927240d56c848503e
csk <shivaranjani229@gmail.com>
2019-05-03 00:09:13 +0530

    push to github

---
+++ src/main/java/sample44/First.java
@@ -0,0 +1,6 @@
+package sample44;
+class First {
+	  public static void main(String[] arguments) {
+	    System.out.println("Let's do something using Java technology.");
+	  }
+	}
---
+++ src/test/java/sample44/hawatest.java
@@ -0,0 +1,14 @@
+package sample44;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class hawatest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
